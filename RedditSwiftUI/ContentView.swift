b575e8b62fc4ab3db6fa74e58ddcf57e8aa79ec4
willtellis <will.ellis@willowtreeapps.com>
2020-11-19 14:40:41 -0500

    Simplify publisher and view model generation

--- RedditSwiftUI/ContentView.swift
+++ RedditSwiftUI/ContentView.swift
@@ -8,39 +8,25 @@
 import SwiftUI
 import Combine
 
+private let postsPublisher: AnyPublisher<[Post], Never> = RedditAPIClient()
+    .postsPublisher(after: nil)
+    .receive(on: DispatchQueue.main)
+    .map { PostsGenerator.make(model: $0) }
+    .catch { Just(PostsGenerator.make(with: $0)) }
+    .eraseToAnyPublisher()
+
 struct ContentView: View {
-    @State private var after: String?
-    @State private var viewModel = ViewModel(posts: [])
-
-    private let afterSubject = PassthroughSubject<String?, Never>()
-    private let apiClient: RedditAPIClient
-    private var viewModelGenerator: ViewModelGenerator
-
-    private var postsPublisher: AnyPublisher<(ViewModel, [IndexPath]), Never>
-
-    init(apiClient: RedditAPIClient = RedditAPIClient(),
-         viewModelGenerator: ViewModelGenerator = ViewModelGenerator()) {
-        self.apiClient = apiClient
-        var viewModelGenerator = viewModelGenerator
-        self.viewModelGenerator = viewModelGenerator
-        self.postsPublisher = apiClient.postsPublisher(after: nil)
-            .receive(on: DispatchQueue.main)
-            .map { viewModelGenerator.make(appending: $0) }
-            .catch { Just(viewModelGenerator.make(with: $0)) }
-            .eraseToAnyPublisher()
-    }
-
-    private var cancellables = Set<AnyCancellable>()
+    @State var posts: [Post] = []
 
     var body: some View {
         NavigationView {
-            List(viewModel.posts) { post in
+            List(posts) { post in
                 Text(post.title ?? "")
                     .padding()
             }
             .navigationTitle("Reddit")
-            .onReceive(postsPublisher, perform: { (viewModel, indexPaths) in
-                self.viewModel = viewModel
+            .onReceive(postsPublisher, perform: { posts in
+                self.posts = posts
             })
         }
     }
@@ -48,6 +34,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(posts: [
+            Post(title: "Test 1"),
+            Post(title: "Test 2")
+        ])
     }
 }
---
+++ RedditSwiftUI/PostsGenerator.swift
@@ -0,0 +1,31 @@
+//
+//  PostsGenerator.swift
+//  RedditSwiftUI
+//
+//  Created by Will Ellis on 11/17/20.
+//
+
+import Foundation
+
+struct PostsGenerator {
+
+    static func make(model: PostsAPIResponse) -> [Post] {
+        return model.data?.children?.compactMap({ makePost(with: $0) }) ?? []
+    }
+
+    static func make(with error: Error) -> [Post] {
+        return []
+    }
+
+    private static func makePost(with model: PostsAPIResponse.Data.Child) -> Post? {
+        return Post(title: model.data?.title)
+    }
+}
+
+struct Post: Identifiable {
+    var id: String? {
+        return title
+    }
+
+    let title: String?
+}
